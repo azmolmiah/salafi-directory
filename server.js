a3e6855e9065741be88f762f286226ba97cb341b
Azmol-Miah <azmol@azmolmiah.com>
2020-04-25 16:12:32 +0100

    Data
    JSON data added for organisations, users, mongodb cluster created and connected

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("./middleware/logger");
 const morgan = require("morgan");
-
-// Router files
-const organisations = require("./routes/organisations");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Router files
+const organisations = require("./routes/organisations");
 
 const app = express();
 
@@ -21,7 +25,14 @@ app.use("/api/v1/organisations", organisations);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
