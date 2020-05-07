0bdc6dac291bb8f8100ad2d6086740edfa8355d0
Azmol-Miah <azmol@azmolmiah.com>
2020-05-07 20:01:50 +0100

    Authentication
    Added login user route and bcrypt validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,5 +16,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ succes: true });
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ succes: true, token });
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ succes: true, token });
 });
