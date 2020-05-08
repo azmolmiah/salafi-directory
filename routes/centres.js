a57451b08a01e5a0e22cc632b6515633a0eb43c5
Azmol-Miah <azmol@azmolmiah.com>
2020-05-08 18:21:35 +0100

    Autorisation
    Added custom messages to authorisaton middleware

--- routes/centres.js
+++ routes/centres.js
@@ -15,7 +15,7 @@ const classRouter = require("./classes");
 
 const router = express.Router();
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
 router.use("/:centreId/classes", classRouter);
@@ -28,12 +28,12 @@ router
     advancedResults(Centre, { path: "classes", select: "title description" }),
     getCentres
   )
-  .post(protect, createCentre);
+  .post(protect, authorize("create", "publisher"), createCentre);
 
 router
   .route("/:id")
   .get(getCentre)
-  .put(protect, updateCentre)
-  .delete(protect, deleteCentre);
+  .put(protect, authorize("update", "publisher"), updateCentre)
+  .delete(protect, authorize("delete", "publisher"), deleteCentre);
 
 module.exports = router;
